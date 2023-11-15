6382069095b99d684bce32df13ae29f7ddc3d711
Stefano <stef.pro@hotmail.com>
2023-11-14 22:06:45 -0500

    chore: update guitar_model file to include more information

--- models/guitar_model.py
+++ models/guitar_model.py
@@ -1,6 +1,14 @@
 # This is subject to change, basic layout will suffice for the time being
 class Guitar:
     def __init__(self):
-        self.brand = ""
-        self.year = ""
-        self.model = ""
+        self.year : int
+        self.brand : str
+        self.model : str
+        self.num_frets : int
+        self.ss_frets : bool
+        self.wood : {
+            "body" : str,
+            "neck" : str,
+            "fretboard" : str
+        }
+        self.locking_tuners : bool
